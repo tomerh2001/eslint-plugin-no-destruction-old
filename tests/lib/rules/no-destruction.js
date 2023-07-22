4c20c3955d299b5cdc90b11c1281bb291c730ee6
Tomer Horowitz <tomerh2001@gmail.com>
2023-07-22 13:16:57 +0300

    Added details to reports

--- tests/lib/rules/no-destruction.js
+++ tests/lib/rules/no-destruction.js
@@ -8,22 +8,44 @@ const ruleTester = new RuleTester({
     },
 });
 
-ruleTester.run("no-destruction", rule, {
-  valid: [
-    "const a = obj.a;",
-    "const b = arr[0];",
-    "let c;",
-    "var d = 1;",
-  ],
-
-  invalid: [
-    {
-      code: "const { a } = obj;",
-      errors: [{ message: "Destructuring is not allowed", type: "VariableDeclarator" }],
-    },
-    {
-      code: "const [b] = arr;",
-      errors: [{ message: "Destructuring is not allowed", type: "VariableDeclarator" }],
-    },
-  ],
+ruleTester.run('no-destruction', rule, {
+    valid: [
+      { 
+        code: `const { a, b } = obj;`, 
+        options: [{ maximumDestructuredVariables: 3, maximumLineLength: 80 }] 
+      },
+      { 
+        code: `const { a } = obj;`, 
+        options: [{ maximumDestructuredVariables: 2, maximumLineLength: 80 }] 
+      },
+      { 
+        code: `const { a, b, c } = obj;`, 
+        options: [{ maximumDestructuredVariables: 3, maximumLineLength: 30 }] 
+      },
+      {
+        code: `function foo() {\n\tconst { a, b } = obj;\n}`,
+        options: [{ maximumDestructuredVariables: 3, maximumLineLength: 80 }]
+      },
+      {
+        code: `\tconst { a, b } = obj;`,
+        options: [{ maximumDestructuredVariables: 3, maximumLineLength: 80 }]
+      }
+    ],
+    invalid: [
+      {
+        code: `const { a, b, c } = obj;`,
+        options: [{ maximumDestructuredVariables: 2, maximumLineLength: 80 }],
+        errors: [{ message: "Destruction of more than 2 variables is not allowed" }]
+      },
+      {
+        code: `const { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z } = obj;`,
+        options: [{ maximumDestructuredVariables: 3, maximumLineLength: 80 }],
+        errors: [{ message: "Destruction of more than 3 variables is not allowed" }, { message: `Destruction on a line exceeding 80 characters is not allowed` }]
+      },
+      {
+        code: `function foo() {\n\t\tconst { a, b } = obj;\n}`,
+        options: [{ maximumDestructuredVariables: 3, maximumLineLength: 80 }],
+        errors: [{ message: `Destruction at a nesting level above 1 is not allowed` }]
+      },
+    ],
 });
